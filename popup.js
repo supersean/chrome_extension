a37c399aa1ef6aae5d9b102c572cb10ec013d098
Sean Davis <sean.davis1289@gmail.com>
2013-06-27 09:17:08 -0500

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+  <script src="popup.js"></script>
+</head>
+
+<body>
+  <h1>My First Javascript</h1>
+  <p id="demo">This is a paragraph.</p>
+  <button type="button">Display Date</button>
+</body>
+</html>
