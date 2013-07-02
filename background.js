1389960a39571e74cb461816d1b96123e1a8a6f8
Sean Davis <sean.davis1289@gmail.com>
2013-07-02 17:30:43 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
 
 function getActiveURL() {
-  return chrome.tabs.getCurrent(function callback() {
+	return chrome.tabs.getCurrent(function callback() {
 		
 	});
 }
 
 this.poop = "poo";
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {	
+	chrome.tabs.get(activeInfo.tabId, function(tab) {
+		localStorage.setItem("last-active-url",tab.url);
+	});
+});
