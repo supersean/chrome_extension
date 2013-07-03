920c8edc6e2f70f77e7f7f7297b2e82ce730ef10
Sean Davis <sean.davis1289@gmail.com>
2013-07-03 17:38:11 -0500

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,7 +1,31 @@
-var ITEM_NAME = 'postingtitle';
+var browser = new function() {
+  this.name = "chrome",
+	this.sendMessage = function(message) {
+		console.log("TESTING");
+		chrome.runtime.sendMessage( { obj : message }, function(response) {
+			console.log(response);
+		});
+	}
+};
 
-var item_name = document.getElementsByClassName(ITEM_NAME).innerHTML;
-console.log(item_name);
+function sendAdInfo() {
+	var obj = { 
+		posting_title : $(".postingtitle").text(),
+		posting_body : $("#postingbody").text(),
+		posting_url : document.URL,
+		type : "ad_info"
+	};
+	
+	browser.sendMessage(obj);
+};
 
-chrome.extension.sendMessage({"name" : "hola"}, function(res) {
-  console.log(res); });
+
+chrome.extension.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		console.log("sup : " + request.type);
+		
+		if(request.type === "ad_info") {
+			sendAdInfo();
+		}
+		sendResponse('Found!');
+	});
